bebc0a1a819f22aec477980cc261a6da79d058ea
John Clark <socrates37@gmail.com>
2015-01-19 11:12:59 -0700

    / returns metadata

--- src/main/java/com/github/trepo/server/application/Root.java
+++ src/main/java/com/github/trepo/server/application/Root.java
@@ -1,5 +1,9 @@
 package com.github.trepo.server.application;
 
+import com.github.trepo.server.factory.VGraphFactory;
+import com.github.trepo.vgraph.VGraph;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.process.internal.RequestScoped;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -9,5 +13,11 @@ public class Root extends ResourceConfig {
 
     public Root() {
         packages("com.github.trepo.server.rest.root", "com.github.trepo.server.provider");
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bindFactory(VGraphFactory.class).to(VGraph.class).in(RequestScoped.class);
+            }
+        });
     }
 }
--- src/main/java/com/github/trepo/server/rest/root/Root.java
+++ src/main/java/com/github/trepo/server/rest/root/Root.java
@@ -5,19 +5,57 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Properties;
 
 /**
+ * Root returns a set of metadata.
  * @author John Clark.
  */
 @Path("/")
 public class Root {
 
+    /**
+     * Gets metadata about this Trepo Server.
+     * @return The metadata.
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response get() {
+
+        Properties properties = getProperties();
+
+        LinkedHashMap<String, String> metadata = new LinkedHashMap<>();
+        metadata.put("name", properties.getProperty("name"));
+        metadata.put("version", properties.getProperty("version"));
+        metadata.put("pTree", properties.getProperty("ptree"));
+        metadata.put("vGraph", properties.getProperty("vgraph"));
+        metadata.put("graph", properties.getProperty("graph"));
+        metadata.put("server", properties.getProperty("server"));
+
+
         return Response
                 .status(Response.Status.OK)
-                .entity("true")
+                .entity(metadata)
                 .build();
+    }
+
+    /**
+     * Loads the generated properties file.
+     * @return The properties file.
+     */
+    public Properties getProperties() {
+        Properties props = new Properties();
+
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream("trepo.properties");
+            props.load(stream);
+        } catch (Exception e) {
+            return props;
+        }
+
+        return props;
     }
 }
