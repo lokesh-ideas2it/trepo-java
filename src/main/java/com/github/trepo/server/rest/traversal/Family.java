3b5f141c81d0a7bab9c0c70566d473c8d73f7a8e
John Clark <socrates37@gmail.com>
2015-05-01 10:25:28 -0600

    hacked in cross-repo query

--- src/main/java/com/github/trepo/server/rest/traversal/Family.java
+++ src/main/java/com/github/trepo/server/rest/traversal/Family.java
@@ -1,8 +1,10 @@
 package com.github.trepo.server.rest.traversal;
 
 import com.github.trepo.npipes.Query;
+import com.github.trepo.npipes.Step;
 import com.github.trepo.npipes.Traversal;
 import com.github.trepo.npipes.TraversalBuilder;
+import com.github.trepo.npipes.gson.StepTypeAdapter;
 import com.github.trepo.ptree.model.core.PersonNameModel;
 import com.github.trepo.ptree.model.what.BirthModel;
 import com.github.trepo.ptree.model.what.DeathModel;
@@ -13,6 +15,11 @@ import com.github.trepo.vgraph.Direction;
 import com.github.trepo.vgraph.Node;
 import com.github.trepo.vgraph.SpecialProperty;
 import com.github.trepo.vgraph.VGraph;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -136,8 +143,46 @@ public class Family {
         query.execute(graph);
 
         // Loop through and execute external requests
-        // TODO
+        HashMap<String, Query> outstandingQueries = new HashMap<>();
+        for (Traversal traversal: query.getTraversals().values()) {
+            if (traversal.isComplete()) {
+                continue;
+            }
+            String repo = traversal.getPath().get(traversal.getPath().size()-1).getRepo();
+            if (!outstandingQueries.containsKey(repo)) {
+                outstandingQueries.put(repo, new Query());
+            }
+            outstandingQueries.get(repo).addTraversal(traversal);
+        }
 
+        if (outstandingQueries.size() > 0) {
+            Gson gson = new GsonBuilder()
+                    .registerTypeHierarchyAdapter(Step.class, new StepTypeAdapter())
+                    .setPrettyPrinting()
+                    .create();
+            for (String repo: outstandingQueries.keySet()) {
+
+                try {
+                    HttpResponse<String> response = Unirest.post(repo + "/npipes/query")
+                            .header("Content-Type", "application/json")
+                            .body(gson.toJson(outstandingQueries.get(repo)))
+                            .asString();
+
+                    //System.out.println(response.getBody());
+
+                    Query returnedQuery = gson.fromJson(response.getBody(), Query.class);
+
+                    for (Traversal returnedTraversal: returnedQuery.getTraversals().values()) {
+                        query.addTraversal(returnedTraversal);
+                    }
+
+                } catch (UnirestException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
 
         // Collate results
         LinkedHashMap<String, Object> family = new LinkedHashMap<>();
@@ -148,8 +193,15 @@ public class Family {
 
         family.put("id", id);
         family.put("repo", person.getRepo());
+        family.put("name", null);
+        family.put("birth", null);
+        family.put("death", null);
 
         for (Traversal traversal: query.getTraversals().values()) {
+            // Skip non complete/finished traversals
+            if (!traversal.isComplete() || traversal.getStep() < traversal.getSteps().size()) {
+                continue;
+            }
             HashMap<String, Object> payload = traversal.getPayload();
             Object typeObject = payload.get("traversal");
             String type = "";
@@ -163,26 +215,56 @@ public class Family {
                     family.put("name", payload.get("name"));
                     break;
                 case "birth":
-                    // TODO
+                    HashMap<String, Object> birth = new HashMap<>();
+                    birth.put("formal", payload.get("formal"));
+                    birth.put("original", payload.get("original"));
+                    birth.put("place", payload.get("place"));
+                    family.put("birth", birth);
                     break;
                 case "death":
-                    // TODO
+                    HashMap<String, Object> death = new HashMap<>();
+                    death.put("formal", payload.get("formal"));
+                    death.put("original", payload.get("original"));
+                    death.put("place", payload.get("place"));
+                    family.put("death", death);
                     break;
                 case "mother":
-                    // TODO
+                    HashMap<String, Object> mother = new HashMap<>();
+                    mother.put("id", payload.get("id"));
+                    mother.put("repo", payload.get("repo"));
+                    mother.put("name", payload.get("name"));
+                    parents.add(mother);
                     break;
                 case "father":
-                    // TODO
+                    HashMap<String, Object> father = new HashMap<>();
+                    father.put("id", payload.get("id"));
+                    father.put("repo", payload.get("repo"));
+                    father.put("name", payload.get("name"));
+                    parents.add(father);
                     break;
                 case "spouse":
-                    // TODO filter out self
+                    if (id.equals(payload.get("id"))) {
+                        continue;
+                    }
+                    HashMap<String, Object> spouse = new HashMap<>();
+                    spouse.put("id", payload.get("id"));
+                    spouse.put("repo", payload.get("repo"));
+                    spouse.put("name", payload.get("name"));
+                    spouses.add(spouse);
                     break;
                 case "child":
-                    // TODO
+                    HashMap<String, Object> child = new HashMap<>();
+                    child.put("id", payload.get("id"));
+                    child.put("repo", payload.get("repo"));
+                    child.put("name", payload.get("name"));
+                    children.add(child);
                     break;
             }
         }
 
+        family.put("parents", parents);
+        family.put("spouses", spouses);
+        family.put("children", children);
         family.put("query", query);
 
         return Response
